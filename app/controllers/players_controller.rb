73749e5c4df0f11ca0aae1a7bb8dd8a8fbf30f60
Stephan Hagemann <stephan.hagemann@gmail.com>
2012-10-05 00:34:42 -0600

    Redirect to games index after create

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -30,7 +30,7 @@ class PlayersController < ApplicationController
 
     respond_to do |format|
       if @player.save
-        format.html { redirect_to @player, notice: 'Player was successfully created.' }
+        format.html { redirect_to games_path, notice: 'Player was successfully created.' }
         format.json { render json: @player, status: :created, location: @player }
       else
         format.html { render action: "new" }
