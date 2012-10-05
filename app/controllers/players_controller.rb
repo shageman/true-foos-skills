c8e9a0cd9310ef6e2a42c5478b7cccbf8f8dd6ad
Stephan Hagemann <stephan.hagemann@gmail.com>
2012-10-05 00:34:37 -0600

    Reducing the api of the players controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -9,25 +9,13 @@ class PlayersController < ApplicationController
   end
 
   # GET /players/1
-  # GET /players/1.json
   def show
     @player = Player.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @player }
-    end
   end
 
   # GET /players/new
-  # GET /players/new.json
   def new
     @player = Player.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @player }
-    end
   end
 
   # GET /players/1/edit
@@ -52,18 +40,13 @@ class PlayersController < ApplicationController
   end
 
   # PUT /players/1
-  # PUT /players/1.json
   def update
     @player = Player.find(params[:id])
 
-    respond_to do |format|
-      if @player.update_attributes(params[:player])
-        format.html { redirect_to @player, notice: 'Player was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @player.errors, status: :unprocessable_entity }
-      end
+    if @player.update_attributes(params[:player])
+      redirect_to @player, notice: 'Player was successfully updated.'
+    else
+      render action: "edit"
     end
   end
 end
