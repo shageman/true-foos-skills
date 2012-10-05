d81f043a55cb2e81f379fb5f5028262dcb3c9dd1
Stephan Hagemann <stephan.hagemann@gmail.com>
2012-10-05 00:34:45 -0600

    Validate player names

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,7 @@
 class Player < ActiveRecord::Base
   attr_accessible :deviation, :mean, :name
   attr_accessor :rating
+
+  validates :name, :length => { :minimum => 3, :maximum => 20 }
+  validates_uniqueness_of :name
 end
